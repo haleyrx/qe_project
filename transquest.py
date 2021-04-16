dd7077640016f0942a56bc512658c79d5913b280
haleyrobyn <hxue42@gatech.edu>
2021-04-16 17:24:51 -0400

    delete py files

--- transquest.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-# Read in train, dev, and test datasets
-df_train = pd.read_csv('./data/en-de/train.ende.df.short.tsv',sep="\t")
-train = df_train[['original', 'translation', 'z_mean']]
-train.head()
